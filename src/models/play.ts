2984a19d60f6b3a4dfec2c78019288076c74bdf7
Unknown <leo.borel.1998@gmail.com>
2018-04-28 06:06:59 +0200

    Added Play

---
+++ src/models/play.ts
@@ -0,0 +1,40 @@
+/**
+ * Dependencies Imports
+ */
+import { Document, model, Model, Schema } from 'mongoose';
+
+/**
+ * Models import
+ */
+
+ import { Quizz, QuizzModel } from './quizz';
+ import { User, UserModel } from './user';
+ 
+ 
+
+/**
+ * PlayModel Type Definition
+ */
+export type PlayModel = Document & {
+    user: UserModel;
+    quizz: QuizzModel;
+};
+
+/**
+ * Play Schema Definition
+ */
+const playSchema: Schema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    quizz: {
+        type: Schema.Types.ObjectId,
+        ref: 'Quizz'
+    }
+});
+
+/**
+ * Play Model Definition
+ */
+export const Play: Model<PlayModel> = model<PlayModel>('Play', playSchema);

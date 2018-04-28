5ca5c4fe7e35cfbec5b686c56ae5a4948fbdd158
Unknown <leo.borel.1998@gmail.com>
2018-04-28 06:02:14 +0200

    Added Level, Question

---
+++ src/models/question.ts
@@ -0,0 +1,34 @@
+/**
+ * Dependencies Imports
+ */
+import { Document, model, Model, Schema } from 'mongoose';
+
+/**
+ * Models import
+ */
+
+ import { Quizz, QuizzModel } from './quizz.ts';
+
+/**
+ * QuestionModel Type Definition
+ */
+export type QuestionModel = Document & {
+    label: string;
+    quizz: QuizzModel;
+};
+
+/**
+ * Question Schema Definition
+ */
+const QuestionSchema: Schema = new Schema({
+    label: String
+    quizz: {
+        type: Schema.Types.ObjectId,
+        ref: 'Quizz'
+    }
+});
+
+/**
+ * Question Model Definition
+ */
+export const Question: Model<QuestionModel> = model<QuestionModel>('Question', QuestionSchema);

6c6028477ef418bd3eb71ca7371e11c4922c95b5
Alexis Bachelet <app.bachelet@gmail.com>
2018-04-28 16:37:46 +0200

    Refactor

--- src/controllers/welcome-controller.ts
+++ src/controllers/welcome-controller.ts
@@ -43,11 +43,15 @@ export class WelcomeController extends AbstractController {
         const faceIds: any = [];
 
         request.post({
-            url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true',
+            url:
+            'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true',
             headers: {
                 'Ocp-Apim-Subscription-Key': '06d4e307942e4c0eaca3c365a9881205'
             },
-            body: JSON.stringify({url: 'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2F80fa3a34-4868-4f22-9312-97bfc8d7367c.2Ejpeg/2216x1536/quality/80/francois-hollande.jpg'})
+            body: JSON.stringify({url:
+'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.'
++ '2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2F80fa3a34-4868-4f22'
++ '-9312-97bfc8d7367c.2Ejpeg/2216x1536/quality/80/francois-hollande.jpg'})
         },
         (err, httpResponse, body) => {
             faceIds.push(JSON.parse(body)[0].faceId);
@@ -56,9 +60,12 @@ export class WelcomeController extends AbstractController {
                 headers: {
                     'Ocp-Apim-Subscription-Key': '06d4e307942e4c0eaca3c365a9881205'
                 },
-                body: JSON.stringify({url: 'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2F80fa3a34-4868-4f22-9312-97bfc8d7367c.2Ejpeg/2216x1536/quality/80/francois-hollande.jpg'})
+                body: JSON.stringify({url:
+                    'https://gal.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image'
++ '.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2F80fa3a34-'
++ '4868-4f22-9312-97bfc8d7367c.2Ejpeg/2216x1536/quality/80/francois-hollande.jpg'})
             },
-            (err, httpResponse, body) => {
+            (err2, httpResponse2, body2) => {
                 faceIds.push(JSON.parse(body)[0].faceId);
 
                 request.post({
@@ -71,7 +78,7 @@ export class WelcomeController extends AbstractController {
                         faceId2: faceIds[1]
                     })
                 },
-                (err, httpResponse, body) => {
+                (err3, httpResponse3, body3) => {
                     faceIds.push(JSON.parse(body));
                     res.send(body);
                 });
